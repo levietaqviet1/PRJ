2a161c95ae63c0e9562580e15239187c5e168f59
viet <85175337+levietaqviet1@users.noreply.github.com>
2022-06-27 23:57:28 +0700

    thua

---
+++ Project_G10_SE1629/src/java/model/Specializedin.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author NCC
+ */
+public class Specializedin {
+
+    private int id;
+    private String name,codeName, dateOfStart, dateOfEnd;
+
+    public Specializedin() {
+    }
+
+    public Specializedin(int id, String name, String codeName, String dateOfStart, String dateOfEnd) {
+        this.id = id;
+        this.name = name;
+        this.codeName = codeName;
+        this.dateOfStart = dateOfStart;
+        this.dateOfEnd = dateOfEnd;
+    }
+
+    public String getCodeName() {
+        return codeName;
+    }
+
+    public void setCodeName(String codeName) {
+        this.codeName = codeName;
+    }
+
+     
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDateOfStart() {
+        return dateOfStart;
+    }
+
+    public void setDateOfStart(String dateOfStart) {
+        this.dateOfStart = dateOfStart;
+    }
+
+    public String getDateOfEnd() {
+        return dateOfEnd;
+    }
+
+    public void setDateOfEnd(String dateOfEnd) {
+        this.dateOfEnd = dateOfEnd;
+    }
+    
+}
